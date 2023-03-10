8846b5ebda854ccdcc6810232b80ad4430bd73ca
Asheikh1 <48831823+Asheikh1@users.noreply.github.com>
2023-03-10 14:42:05 +0000

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Test')
+streamlit.title('My parents healthy breakfast')
+streamlit.header('Breakfast menu')
+streamlit.text('Blueberry pancakes')
+streamlit.text('Omlette')
+streamlit.text('Banana strawberry smoothie')
+streamlit.text('French toast')
